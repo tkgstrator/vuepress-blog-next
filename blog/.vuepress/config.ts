a5920f4e543c26301b09812878ab11168d981bc2
tkgstrator <nasawake.am@gmail.com>
2023-06-12 11:16:40 +0900

    comment feature

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -1,26 +1,44 @@
 import { defineUserConfig } from 'vuepress'
-import { hopeTheme } from "vuepress-theme-hope";
+import { comment, hopeTheme } from "vuepress-theme-hope";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import { searchPlugin } from "@vuepress/plugin-search";
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  lang: 'en-US',
-  title: 'えいむーと愉快な仲間たち',
-  description: '洗ってないワンチャンの臭いがします',
+  lang: "en-US",
+  title: "えいむーと愉快な仲間たち",
+  description: "洗ってないワンチャンの臭いがします",
   head: [
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap' }],
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap",
+      },
+    ],
   ],
   plugins: [
     searchPlugin({
       maxSuggestions: 10,
-    })
+    }),
   ],
   theme: hopeTheme({
+    plugins: {
+      comment: {
+        provider: "Giscus",
+        repo: "tkgstrator/vuepress-blog-next",
+        repoId: "R_kgDOJuLbqA",
+        category: "Announcements",
+        categoryId: "DIC_kwDOJuLbqM4CXI-M",
+        lazyLoading: true,
+        reactionsEnabled: true,
+        delay: 1,
+      },
+    },
   }),
   bundler: viteBundler({
     viteOptions: {},
     vuePluginOptions: {},
-  })
-})
+  }),
+});
